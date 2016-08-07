133e59a59cea83f9883271182cfef4a53bf1fb32
Takayuki MATSUOKA <takayuki.matsuoka@gmail.com>
2016-08-08 06:24:22 +0900

    Add gameMain for gunhound

---
+++ gameMain.cpp
@@ -0,0 +1,43 @@
+/*
+
+  GameMain.cpp
+
+  written by ragi.
+
+  2016.06.28
+*/
+#include <gxLib.h>
+
+
+gxBool GameMain()
+{
+	gxBool GunHoundMain(gxBool bReset);
+	GunHoundMain( gxFalse );
+	return gxTrue;
+}
+
+gxBool GameEnd()
+{
+	return gxTrue;
+}
+
+gxBool GameSleep()
+{
+	return gxTrue;
+}
+
+gxBool GameResume()
+{
+	return gxTrue;
+}
+
+gxBool GameReset()
+{
+	return gxTrue;
+}
+
+gxBool DragAndDrop(char* szFileName)
+{
+	return gxTrue;
+}
+
