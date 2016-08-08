e04825c435de81b3a33ccd01f62f75b145415ba3
Takayuki MATSUOKA <takayuki.matsuoka@gmail.com>
2016-08-08 10:19:09 +0900

    Fix build problem
    - Add libogg.props
    - Fix xcopy's switches

--- external/build-oggvorbis.cmd
+++ external/build-oggvorbis.cmd
@@ -2,8 +2,8 @@
 pushd "%~dp0"
 
 xcopy /IEY .\libogg-1.3.2\include\ogg .\libvorbis-1.3.5\include\ogg
-xcopy /IEY .\VS2015\libogg-1.3.2\ .\libogg-1.3.2\
-xcopy /IEY .\VS2015\libvorbis-1.3.5\ .\libvorbis-1.3.5\
+xcopy /IEY .\VS2015\libogg-1.3.2 .\libogg-1.3.2
+xcopy /IEY .\VS2015\libvorbis-1.3.5 .\libvorbis-1.3.5
 
 call "%VS140COMNTOOLS%"vsvars32.bat
 MSBuild .\libogg-1.3.2\win32\vs2015\libogg_static.sln /p:Configuration=Release /p:Platform=x64
